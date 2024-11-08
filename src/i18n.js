809ee6af15b41f696bc9b9784bb10c62be62b8b6
thanhtd <thanhtd@vn.sprite.jp>
2024-11-08 11:20:43 +0700

    add multi languages

--- src/i18n.js
+++ src/i18n.js
@@ -1,10 +1,12 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import resourcesToBackend from 'i18next-resources-to-backend';
+import LanguageDetector from 'i18next-browser-languagedetector'; // Import language detector
 
 // Initialize i18next
 i18n
-  .use(initReactI18next) // if using with React
+  .use(LanguageDetector) // Add the language detection plugin
+  .use(initReactI18next) // If using with React
   .use(resourcesToBackend((language, namespace) => import(`./locales/${language}/${namespace}.json`)))
   .init({
     lng: 'en', // default language
@@ -14,6 +16,12 @@ i18n
     interpolation: {
       escapeValue: false, // React already handles escaping
     },
+    detection: {
+      // Methods to detect language in order
+      order: ['querystring', 'cookie', 'localStorage', 'navigator', 'htmlTag'],
+      caches: ['localStorage', 'cookie'], // Save detected language in cookie and localStorage
+    },
   });
 
 export default i18n;
+
