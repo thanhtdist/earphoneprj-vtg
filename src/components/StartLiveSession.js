5cffd3eee1348a311bb00eeb81b5e6465d706c19
thanhtd <thanhtd@vn.sprite.jp>
2024-11-29 11:40:38 +0700

    improve translate voice

--- src/components/StartLiveSession.js
+++ src/components/StartLiveSession.js
@@ -222,7 +222,6 @@ function StartLiveSession() {
     console.log("enableLiveTranscription meetingId", meetingSession.configuration.meetingId);
     //const language = localStorage.getItem('i18nextLng');
     const languageCode = i18n.language === 'ja' ? "ja-JP" : "en-US";
-    console.log("current language", language);
     console.log("current languageCode", languageCode);
     const startMeetingTranscriptionResponse = await startMeetingTranscription(meetingSession.configuration.meetingId, languageCode);
     console.log("enableLiveTranscription startMeetingTranscriptionResponse", startMeetingTranscriptionResponse);
@@ -313,8 +312,8 @@ function StartLiveSession() {
       setMicroChecking('microChecking');
 
       // Check if there are no devices or if any device label is empty
-      //if (devices.length === 0 || devices.some(device => !device.label.trim())) {
-      if (devices.length === 0) {
+      if (devices.length === 0 || devices.some(device => !device.label.trim())) {
+      //if (devices.length === 0) {
         console.log('No audio input devices found');
         // Display a message after 5 seconds
         setTimeout(() => {
