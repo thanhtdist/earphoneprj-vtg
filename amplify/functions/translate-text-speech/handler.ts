e080cae3dfc6135328fafb34579d971fcdef1df8
thanhtd <thanhtd@vn.sprite.jp>
2024-12-05 17:45:10 +0700

    improve translate voice

--- amplify/functions/translate-text-speech/handler.ts
+++ amplify/functions/translate-text-speech/handler.ts
@@ -53,7 +53,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       OutputFormat: 'mp3',
       Text: translateTextResponse.TranslatedText,
       //VoiceId: 'Mizuki' // Mizuki for a female voice. Takumi for a male voice.
-      VoiceId: targetLanguageCode === 'ja-JP' ? 'Mizuki' : targetLanguageCode === 'en-US' ? 'Danielle' : 'Joanna'
+      VoiceId: targetLanguageCode === 'ja-JP' ? 'Mizuki' : targetLanguageCode === 'en-US' ? 'Joanna' : targetLanguageCode === 'ko-KR' ? 'Seoyeon': 'Joanna'
     }).promise();
 
     // Return successful response
