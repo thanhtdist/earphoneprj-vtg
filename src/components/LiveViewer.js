9a17be8a66a717c6d814224d4b44e186b3b4dd11
thanhtd <thanhtd@vn.sprite.jp>
2024-12-10 17:09:12 +0700

    improve translate voice

--- src/components/LiveViewer.js
+++ src/components/LiveViewer.js
@@ -429,9 +429,9 @@ function LiveViewer() {
               {LISTEN_VOICE_LANGUAGES.find((lang) => lang.key === selectedVoiceLanguage)?.label}.
             </p>
             <br />
-            {transcriptListRef.current.length > 0 && (
+            {transcriptedListRef.current.length > 0 && (
               <span>
-                Transcriptions: <span>{transcriptListRef.current.join(' ')}</span>
+                Transcriptions: <span>{transcriptedListRef.current.join(' ')}</span>
               </span>
             )}
             <br />
