ba9dfe9d126e79c019b62ee2a547378765c30fcb
thanhtd <thanhtd@vn.sprite.jp>
2025-04-22 13:55:39 +0700

    research audio buzzing voice

--- src/components/LiveViewer2.js
+++ src/components/LiveViewer2.js
@@ -101,6 +101,8 @@ function LiveViewer2() {
       //const audioElement = document.getElementById('audioElementListener');
       const audioElement = audioElementRef.current;
       console.log('Check audioElement:', audioElement);
+      const currentMeetingAudioStream = session.audioVideo.getCurrentMeetingAudioStream();
+      console.log('Check currentMeetingAudioStream:', currentMeetingAudioStream);
       if (audioElement) {
         console.log('Audio element found:', audioElement);
         alert('Audio element found!');
