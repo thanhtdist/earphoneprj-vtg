54248f4d3abc05178760fe319b5595ecc830d148
thanhtd <thanhtd@vn.sprite.jp>
2025-04-23 10:53:18 +0700

    research audio buzzing voice

--- src/components/LiveViewer2.js
+++ src/components/LiveViewer2.js
@@ -193,28 +193,28 @@ function LiveViewer2() {
     //     // Xử lý khi stream im lặng
     //   }
     // });
-    session.audioVideo.subscribeToActiveSpeakerDetector(
-      // Callback list of attendees sorted by activity (most active first)
-      (activeSpeakers) => {
-        alert('Active speakers:', activeSpeakers);
-        console.log("Active Speakers:", activeSpeakers);
-        // You can highlight the first active speaker in UI
-      },
-      // Activity score callback
-      (attendeeId, score) => {
-        alert('Activity score:', score);
-        alert('Attendee ID:', attendeeId);
-        // You can use this score to determine if the attendee is active or not
-        console.log(`Activity score for ${attendeeId}: ${score}`);
-      },
-      // Optional config
-      {
-        intervalMs: 1000,       // how often to check activity
-        scoreCallbackIntervalMs: 1000,
-        speakerThreshold: 0.6,  // adjust this sensitivity
-        silenceThreshold: 0.01, // when everyone is quiet
-      }
-    );
+    // session.audioVideo.subscribeToActiveSpeakerDetector(
+    //   // Callback list of attendees sorted by activity (most active first)
+    //   (activeSpeakers) => {
+    //     alert('Active speakers:', activeSpeakers);
+    //     console.log("Active Speakers:", activeSpeakers);
+    //     // You can highlight the first active speaker in UI
+    //   },
+    //   // Activity score callback
+    //   (attendeeId, score) => {
+    //     alert('Activity score:', score);
+    //     alert('Attendee ID:', attendeeId);
+    //     // You can use this score to determine if the attendee is active or not
+    //     console.log(`Activity score for ${attendeeId}: ${score}`);
+    //   },
+    //   // Optional config
+    //   {
+    //     intervalMs: 1000,       // how often to check activity
+    //     scoreCallbackIntervalMs: 1000,
+    //     speakerThreshold: 0.6,  // adjust this sensitivity
+    //     silenceThreshold: 0.01, // when everyone is quiet
+    //   }
+    // );
     session.audioVideo.start();
 
   }, [selectedVoiceLanguage]);
