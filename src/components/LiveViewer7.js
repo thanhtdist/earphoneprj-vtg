f87d920772e5e72bee0ae24b1b7b4ded7766f6d3
thanhtd <thanhtd@vn.sprite.jp>
2025-05-23 16:34:57 +0700

    process only japanese audio

--- src/components/LiveViewer7.js
+++ src/components/LiveViewer7.js
@@ -22,13 +22,13 @@ import '../styles/LiveViewer.css';
 import { useTranslation } from 'react-i18next';
 import { LISTEN_VOICE_LANGUAGES, JA_LISTEN_VOICE_LANGUAGES } from '../utils/constant';
 import Header from './Header';
-// import { HiMiniSpeakerWave } from "react-icons/hi2";
-// import { IoVolumeMute } from "react-icons/io5";
+import { HiMiniSpeakerWave } from "react-icons/hi2";
+import { IoVolumeMute } from "react-icons/io5";
 //import MessageBox from './MessageBox';
 import { useParams } from "react-router-dom";
 import NotFound from './NotFound';
 import TourTitle from './TourTitle';
-// import { FaPause, FaPlay } from "react-icons/fa";
+import { FaPause, FaPlay } from "react-icons/fa";
 import { GUIDE_NOT_START } from '../utils/messages';
 
 function LiveViewer7() {
@@ -37,19 +37,19 @@ function LiveViewer7() {
   console.log('tourId:', tourId);
   const { t, i18n } = useTranslation();
   const [tour, setTour] = useState(undefined);
-  // const [meetingSession, setMeetingSession] = useState(null);
+  //const [meetingSession, setMeetingSession] = useState(null);
   // const [meeting, setMeeting] = useState(null);
   // const [attendee, setAttendee] = useState(null);
   //const [channelArn, setChannelArn] = useState('');
   //const [userArn, setUserArn] = useState('');
   const [isLoading, setIsLoading] = useState(false);
-  // const [participantsCount, setParticipantsCount] = useState(0);
+  //const [participantsCount, setParticipantsCount] = useState(0);
   const [selectedVoiceLanguage, setSelectedVoiceLanguage] = useState(
     LISTEN_VOICE_LANGUAGES.find((lang) => lang.key.startsWith(i18n.language))?.key || 'ja-JP'
   );
   // const [chatRestriction, setChatRestriction] = useState(null);
-  // const [isMuted, setIsMuted] = useState(true);
-  // const [isPlay, setIsPlay] = useState(false);
+  const [isMuted, setIsMuted] = useState(true);
+  const [isPlay, setIsPlay] = useState(false);
   // const userID = uuidv4();
   const userType = 'User';
   // Ref for the audio element  
@@ -119,6 +119,24 @@ function LiveViewer7() {
   //   }
   // }, []);
 
+  // Event for handling mute/unmute button click
+  const handleMuteUnmute = () => {
+    setIsMuted(!isMuted);
+    audioElementRef.current.muted = isMuted;
+  };
+
+  // Function to handle play/pause button click
+  const handlePlay = () => {
+    console.log('xCheck audioElement:', audioElementRef.current);
+    if (isPlay === false) {
+      setIsPlay(true)
+      audioElementRef.current.play();
+    } else {
+      setIsPlay(false);
+      audioElementRef.current.pause();
+    }
+  }
+
   // Function intialize Meeting Session
   const initializeMeetingSession = useCallback(async (meetingData, attendeeData) => {
     if (!meetingData || !attendeeData) {
@@ -129,6 +147,7 @@ function LiveViewer7() {
     const deviceController = new DefaultDeviceController(logger);
     const meetingSessionConfig = new MeetingSessionConfiguration(meetingData, attendeeData);
     const session = new DefaultMeetingSession(meetingSessionConfig, logger, deviceController);
+    //setMeetingSession(session);
 
     // Create log collection for aggregated reporting
     const sessionLogs = [];
@@ -233,9 +252,29 @@ function LiveViewer7() {
     //   addLog('ERROR', 'Audio element not found');
     // }
 
+    session.audioVideo.realtimeSubscribeToVolumeIndicator(
+      (attendeeId, volume, muted, signalStrength) => {
+        console.log('Volume:', volume);
+        if (volume > 0) {
+          alert(`xVolume>0 ${Math.round(volume * 100)}% from ${attendeeId.substring(0, 8)}...`, { muted, signalStrength });
+        } else {
+          alert(`yVolume ${Math.round(volume * 100)}% from ${attendeeId.substring(0, 8)}...`, { muted, signalStrength });
+        }
+      }
+    );
+
     if (session.audioVideo) {
       session.audioVideo.start();
       addLog('CONNECTION', 'AudioVideo session started');
+      // session.audioVideo.subscribeToActiveSpeakerDetector(
+      //   (attendeeId, isActiveSpeaker) => {
+      //     if (isActiveSpeaker) {
+      //       alert(`Active speaker: ${attendeeId}`);
+      //     } else {
+      //       alert(`Inactive speaker: ${attendeeId}`);
+      //     }
+      //   }
+      // );
     }
 
     return session; // Return the session in case you want to store it
@@ -328,9 +367,29 @@ function LiveViewer7() {
   //   // Subscribe to attendee presence
   //   meetingSession.audioVideo.realtimeSubscribeToAttendeeIdPresence(presenceCallback);
 
+  //   meetingSession.audioVideo.realtimeSubscribeToVolumeIndicator(
+  //     (attendeeId, volume, muted, signalStrength) => {
+  //       if (volume > 0) {
+  //         alert(`xVolume>0 ${Math.round(volume * 100)}% from ${attendeeId.substring(0, 8)}...`, { muted, signalStrength });
+  //       } else {
+  //         console.log(`yVolume ${Math.round(volume * 100)}% from ${attendeeId.substring(0, 8)}...`, { muted, signalStrength });
+  //       }
+  //     }
+  //   );
+
+  //   meetingSession.audioVideo.subscribeToActiveSpeakerDetector(
+  //     (attendeeId, isActiveSpeaker) => {
+  //       if (isActiveSpeaker) {
+  //         console.log(`Active speaker: ${attendeeId}`);
+  //       } else {
+  //         console.log(`Inactive speaker: ${attendeeId}`);
+  //       }
+  //     }
+  //   );
+
   //   // Cleanup on unmount
   //   return () => {
-  //     meetingSession.audioVideo.realtimeUnsubscribeFromAttendeeIdPresence(presenceCallback);
+  //     //meetingSession.audioVideo.realtimeUnsubscribeFromAttendeeIdPresence(presenceCallback);
   //   };
   // }, [meetingSession]);
 
@@ -411,7 +470,7 @@ function LiveViewer7() {
               </div>
             ) : (<>
               <TourTitle tour={tour} />
-              {/* <div className='audioViewer'>
+              <div className='audioViewer'>
                 {!!isPlay ? <div>
                   <div className='pauseButtonViewer' onClick={handlePlay}>
                     <FaPause size={20} />
@@ -428,7 +487,7 @@ function LiveViewer7() {
                   {isMuted ? <HiMiniSpeakerWave size={30} /> : <IoVolumeMute size={30} />
                   }
                 </div>
-              </div> */}
+              </div>
               {/* {chatRestriction !== "nochat" && (<MessageBox userArn={userArn} sessionId={Config.sessionId} channelArn={channelArn} userType={userType} statusChat={chatRestriction} />)} */}
             </>)}
 
