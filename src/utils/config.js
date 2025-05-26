268e9f17c219f812b5970cd53c2fb56a0f557b16
thanhtd <thanhtd@vn.sprite.jp>
2025-05-26 18:00:07 +0700

    update config

--- src/utils/config.js
+++ src/utils/config.js
@@ -18,9 +18,9 @@ const Config = {
     // appGuideURL: 'http:localhost:3000/guide',
     // appSubGuideURL: 'http://localhost:3000/sub-guide',
     // appViewerURL: 'http://localhost:3000/viewer',
-    appGuideURL: 'https://vtg-admin.d2rl061pfg6j3s.amplifyapp.com/guide',
-    appSubGuideURL: 'https://vtg-admin.d2rl061pfg6j3s.amplifyapp.com/sub-guide',
-    appViewerURL: 'https://vtg-admin.d2rl061pfg6j3s.amplifyapp.com/viewer',
+    appGuideURL: 'https://earphoneguide.d1mxns0xu1qyis.amplifyapp.com/guide',
+    appSubGuideURL: 'https://earphoneguide.d1mxns0xu1qyis.amplifyapp.com/sub-guide',
+    appViewerURL: 'https://earphoneguide.d1mxns0xu1qyis.amplifyapp.com/viewer',
     // S3 attchment bucket name
     attachmentBucketName: 'vtg-chat-attachments',
     // Write logs from client side to CloudWatch using API Gateway      
