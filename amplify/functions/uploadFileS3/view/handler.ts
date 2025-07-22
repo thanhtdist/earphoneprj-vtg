22e74cba31f1ea9be168bf6d365ab8575911f11f
thanhtd <thanhtd@vn.sprite.jp>
2025-07-22 10:50:19 +0700

    update chat message

--- amplify/functions/uploadFileS3/view/handler.ts
+++ amplify/functions/uploadFileS3/view/handler.ts
@@ -1,7 +1,6 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { getSignedUrl } from "@aws-sdk/cloudfront-signer";
 import { Config } from '@configs/config';
-import { secret } from '@aws-amplify/backend';
 
 const CLOUDFRONT_DOMAIN = 'https://d8d9ccu87krcw.cloudfront.net'; // replace with yours
 const KEY_PAIR_ID = 'KM5PK06K9XZUQ'; // replace with yours
@@ -17,8 +16,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
         headers: Config.headers,
       };
     }
-    const testPrivateKey = secret('PRIVATE_KEY');
-    console.log("Test Private Key:", testPrivateKey);
+    console.log("File Key:", fileKey);
     console.log("Private Key:", Config.privateKey);
 
     const url = `${CLOUDFRONT_DOMAIN}/${fileKey}`;
