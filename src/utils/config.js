8bef22f00df288e630a5a50265e3708725422e4b
thanhtd <thanhtd@vn.sprite.jp>
2025-08-04 13:33:22 +0700

    add max connection

--- src/utils/config.js
+++ src/utils/config.js
@@ -40,7 +40,7 @@ const Config = {
     },
 
     // Websocket URL for real-time translate audio
-    webSocketURL: 'wss://4cfepibmyh.execute-api.us-east-1.amazonaws.com/prod',
+    webSocketURL: 'wss://tnmrusql0d.execute-api.us-east-1.amazonaws.com/prod',
     
     /**
      * Storage and Logging Configuration
